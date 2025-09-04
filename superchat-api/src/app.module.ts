6dc83b9fda7affb2975da68db9858381669fef79
victorzix <linkgamesplays@gmail.com>
2025-09-04 15:28:31 -0300

    feat(messages:queue): add bullmq module

--- superchat-api/src/app.module.ts
+++ superchat-api/src/app.module.ts
@@ -9,6 +9,7 @@ import { ChatModule } from '@/chat/chat.module';
 import { WebSocketModule } from '@/websocket/websocket.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { redisStore } from 'cache-manager-redis-yet';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
@@ -42,6 +43,13 @@ import { redisStore } from 'cache-manager-redis-yet';
           password: process.env.REDIS_MESSAGES_PASSWORD,
         }),
       }),
+    }),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_QUEUE_HOST,
+        port: +process.env.REDIS_QUEUE_PORT,
+        password: process.env.REDIS_QUEUE_PASSWORD,
+      },
     }),
     ChatModule,
     WebSocketModule,
